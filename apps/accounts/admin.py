a2026e824e082f815c9ac391e26c2af501788aaa
kashifnadeemeng <kashifnadeem.eng@gmail.com>
2020-08-11 19:44:49 +0500

    Create models to manage inventory

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from apps.books.models import Category, Book
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+
+
+class BookAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ('title', 'price', 'author', 'is_featured')
+    search_fields = ('title', 'author', 'is_featured')
+    list_filter = ('category', 'author', 'is_featured')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
