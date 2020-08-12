e337ffc307ee3c7a5db3879562426d91e928ed70
kashifnadeemeng <kashifnadeem.eng@gmail.com>
2020-08-12 19:47:07 +0500

    Mention chanches are applied.

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -2,16 +2,13 @@ from django.contrib import admin
 from apps.books.models import Category, Book
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'updated_at')
 
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    model = Category
-    list_display = ('title', 'price', 'author', 'is_featured')
+    list_display = ('category', 'title', 'price', 'author', 'is_featured')
     search_fields = ('title', 'author', 'is_featured')
     list_filter = ('category', 'author', 'is_featured')
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Book, BookAdmin)
