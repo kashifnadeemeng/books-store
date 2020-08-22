340884c4fa6b7e45367523dada3b8d4c849b41e6
kashifnadeemeng <kashifnadeem.eng@gmail.com>
2020-08-22 15:11:23 +0500

    All is done.

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,5 +1,10 @@
-from .models import Category, Book
-from django.views.generic import ListView, DetailView
+from .models import Book, Category
+from django.views.generic import DetailView, ListView
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category_detail.html'
 
 
 class CategoryListView(ListView):
@@ -7,13 +12,6 @@ class CategoryListView(ListView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
         context['featured_books'] = Book.objects.featured()
         return context
-
-
-class CategoryDetailView(DetailView):
-    model = Category
-    template_name = 'category_books.html'
